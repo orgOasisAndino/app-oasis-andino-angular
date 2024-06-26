d08f38cc348b48c7f616a3d15f66242b9cacc091
jhimydevgL <chance.jhimy@gmail.com>
2024-06-26 14:53:30 -0400

    hito 1

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLinkWithHref,RouterLinkActive],
+  imports: [RouterLinkWithHref,RouterLinkActive,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 
 }
