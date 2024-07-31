bf9446d6ac78af865c396d21d52c462589085e75
Jhosselyn30 <astridjhosselyn@gmail.com>
2024-07-31 18:39:02 -0400

    Primera versión responsive la sección pomabamba

--- src/app/domains/info/pages/pomabamba/pomabamba.component.ts
+++ src/app/domains/info/pages/pomabamba/pomabamba.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-pomabamba',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './pomabamba.component.html',
   styleUrl: './pomabamba.component.css'
 })
 export class PomabambaComponent {
+  mostrarSeccion: boolean;
 
+  constructor() {
+    // Define la lógica para determinar si mostrar la sección
+    this.mostrarSeccion = this.determinarSiMostrarSeccion();
+  }
+
+  determinarSiMostrarSeccion(): boolean {
+    // Aquí puedes poner la lógica que necesites
+    // Por ejemplo, podrías basarlo en el tamaño de la ventana
+    const width = window.innerWidth;
+    return width >= 768; // Por ejemplo, muestra solo en pantallas md o más grandes
+  }
 }
